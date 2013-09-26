1c17162fd8621665e99e6d8c03dfb8262f20cd0d
meinhard <meinhard@benn.org>
2013-09-27 00:15:16 +0200

    add simple test with dataProvider

---
+++ src/Acme/DemoBundle/Tests/DemoTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Acme\DemoBundle\Tests;
+
+class DemoTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @dataProvider getNumbers
+     */
+    public function testSum($num1, $num2, $sum)
+    {
+        $this->assertEquals($sum, $num1 + $num2);
+    }
+
+    public function getNumbers()
+    {
+        return array(
+            array(2, 3, 5),
+            array(1, 7, 8),
+            array(4, 1, 6)
+        );
+    }
+}
