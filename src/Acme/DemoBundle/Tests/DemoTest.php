90e9322b86ec51294557cccc533658fe5c281274
meinhard <meinhard@benn.org>
2013-09-27 13:16:20 +0200

    i fixed it, hehe

--- src/Acme/DemoBundle/Tests/DemoTest.php
+++ src/Acme/DemoBundle/Tests/DemoTest.php
@@ -12,15 +12,12 @@ class DemoTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals($sum, $num1 + $num2);
     }
 
-    /**
-     * TODO: fixme, we are waiting!!!111
-     */
     public function getNumbers()
     {
         return array(
             array(2, 3, 5),
             array(1, 7, 8),
-            array(4, 1, 6)
+            array(4, 1, 5)
         );
     }
 }
